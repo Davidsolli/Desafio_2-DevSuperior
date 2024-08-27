a4b10815f0ae92645f5326c77f67b96a8d569f42
Davidsolli <davidolisoa@gmail.com>
2024-08-27 11:15:41 -0300

    busca paginada e post de atividades

--- src/main/java/com/davidsolli/desafio2/controller/AtividadeController.java
+++ src/main/java/com/davidsolli/desafio2/controller/AtividadeController.java
@@ -3,11 +3,13 @@ package com.davidsolli.desafio2.controller;
 import com.davidsolli.desafio2.DTO.AtividadeDTO;
 import com.davidsolli.desafio2.services.AtividadeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/atividade")
@@ -20,5 +22,18 @@ public class AtividadeController {
     public ResponseEntity<AtividadeDTO> findById(@PathVariable Integer id) {
         AtividadeDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<AtividadeDTO>> findAll(Pageable pageable) {
+        Page<AtividadeDTO> result = service.findAll(pageable);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping
+    public ResponseEntity<AtividadeDTO> insert(@RequestBody AtividadeDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
--- src/main/java/com/davidsolli/desafio2/services/AtividadeService.java
+++ src/main/java/com/davidsolli/desafio2/services/AtividadeService.java
@@ -4,6 +4,8 @@ import com.davidsolli.desafio2.DTO.AtividadeDTO;
 import com.davidsolli.desafio2.entities.Atividade;
 import com.davidsolli.desafio2.repositories.AtividadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +19,25 @@ public class AtividadeService {
     public AtividadeDTO findById(Integer id) {
         Atividade atividade = repository.findById(id).get();
         return new AtividadeDTO(atividade);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<AtividadeDTO> findAll(Pageable pageable) {
+        Page<Atividade> result = repository.findAll(pageable);
+        return result.map(x -> new AtividadeDTO(x));
+    }
+
+    @Transactional
+    public AtividadeDTO insert(AtividadeDTO dto) {
+        Atividade entity = new Atividade();
+        saveEntity(entity, dto);
+        entity = repository.save(entity);
+        return new AtividadeDTO(entity);
+    }
+
+    public void saveEntity(Atividade entity, AtividadeDTO dto) {
+        entity.setNome(dto.getNome());
+        entity.setDescricao(dto.getDescricao());
+        entity.setPreco(dto.getPreco());
     }
 }
